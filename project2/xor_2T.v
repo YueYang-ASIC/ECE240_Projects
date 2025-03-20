9611bb6b5396e7146a433d2c61c942f6354e62d9
YUEYANG-ASIC <lunayangwork@gmail.com>
2025-03-20 16:40:02 +0000

    6T_update

--- project2/xor_2T.v
+++
@@ -1,8 +0,0 @@
-module xor_2T (output XxorY, input X, input X_bar, input Y);
-  wire net1,net2;
-  // Pass transistor logic
- /   nmos1v (net1, X_bar, Y);    // NMOS transistor: Y controls passing X_bar
-     pmos1v (net2, X, Y); // PMOS transistor: Y controls passing the complement
-  assign XxorY = net1 | net2;
- endmodule
- 
